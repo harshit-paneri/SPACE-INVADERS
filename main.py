7cfdf6c3d93f2ac82d859e2111d89a16c91a53b6
Harshit Paneri <harshitpanari@gmail.com>
2022-05-29 15:06:06 +0530

    adding background img

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ pygame.init()
 # screen create
 screen = pygame.display.set_mode((800, 600))
 
+#background
+background = pygame.image.load('background.png')
+
 
 # Title and icon
 pygame.display.set_caption("SPACE INVADERS")
@@ -25,7 +28,7 @@ playerX_change = 0
 enemyImg = pygame.image.load('alien.png')
 enemyX = random.randint(0, 800)
 enemyY = random.randint(50, 150)
-enemyX_change = 0.2
+enemyX_change = 2
 enemyY_change = 40
 
 
@@ -46,6 +49,8 @@ while running:
 
     #RGB (0,0,0)
     screen.fill((0, 0, 0))
+    #background image
+    screen.blit(background, (0, 0))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -55,9 +60,9 @@ while running:
         if event.type == pygame.KEYDOWN:
             #print("A keystoke is pressed")
             if event.key == pygame.K_LEFT:
-                playerX_change = -0.3
+                playerX_change = -5
             if event.key == pygame.K_RIGHT:
-                playerX_change = 0.3
+                playerX_change = 5
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerX_change = 0
@@ -74,13 +79,13 @@ while running:
     enemyX += enemyX_change
 
     if enemyX <= 0:
-        enemyX_change = 0.2
+        enemyX_change = 2
         enemyY += enemyY_change
     elif enemyX >= 736:
-        enemyX_change = -0.2
+        enemyX_change = -2
         enemyY += enemyY_change
 
     player(playerX, playerY)
     enemy(enemyX, enemyY)
     pygame.display.update()
-    
+
