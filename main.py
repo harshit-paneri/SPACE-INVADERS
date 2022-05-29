74af8d213c6d104cee50285e1131dcd43d87d64f
Harshit Paneri <harshitpanari@gmail.com>
2022-05-29 09:06:10 +0530

    screen set

--- main.py
+++ main.py
@@ -5,7 +5,13 @@ import pygame
 pygame.init()
 
 #screen create
-Screen = pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((800, 600))
 
-while True:
-    pass
+
+
+#Game loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
