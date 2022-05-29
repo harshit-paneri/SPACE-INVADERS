0914420b077da73e85327f494d48df8c318df21c
Harshit Paneri <harshitpanari@gmail.com>
2022-05-29 09:38:41 +0530

    player postion set

--- main.py
+++ main.py
@@ -14,10 +14,26 @@ pygame.display.set_caption("SPACE INVADERS")
 Icon = pygame.image.load('spaceshipgame.png')
 pygame.display.set_icon(Icon)
 
+#player
+playerImg = pygame.image.load('space-invaders.png')
+playerX = 370
+playerY = 480
+
+def player():
+    screen.blit(playerImg ,(playerX, playerY))
 
 #Game loop
 running = True
 while running:
+    
+    #RGB (0,0,0)
+    screen.fill((0,0,0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    
+
+    player()
+    pygame.display.update()
