754ac5fc6a607a5a2b4c5ca6fbb0a028d9a97312
Harshit Paneri <harshitpanari@gmail.com>
2022-05-29 09:17:29 +0530

    add icon with tite bar

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from msilib.schema import Icon
 from turtle import Screen
 import pygame
 
@@ -7,6 +8,11 @@ pygame.init()
 #screen create
 screen = pygame.display.set_mode((800, 600))
 
+
+# Title and icon
+pygame.display.set_caption("SPACE INVADERS")
+Icon = pygame.image.load('spaceshipgame.png')
+pygame.display.set_icon(Icon)
 
 
 #Game loop
